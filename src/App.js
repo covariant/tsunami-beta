2f45a0bf43754619c3a93917e27cf39fcd755a76
covariant <raxarib590@xeiex.com>
2021-10-07 21:27:31 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <div className="App-article">
+          <header className="App-header">
+            Tsunami App
+          </header>
+          <div className="App-section">WIP
+          </div>
+          <footer className="App-footer">Solana Ignition
+          </footer>
+        </div>
+      </body>
     </div>
   );
 }
